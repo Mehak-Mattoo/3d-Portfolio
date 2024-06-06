9c4e0107ea23b7c0af31013139a1e8cacf8e1f66
Mehak <mehakmattoo7@gmail.com>
2024-06-06 16:50:48 +0530

    Home section completed

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -1,7 +1,6 @@
 import { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-
 import skyScene from "../assets/3d/sky.glb";
 
 const Sky = ({ isRotating }) => {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Island from "../models/Island";
 import Sky from "../models/Sky";
 import Bird from "../models/Bird";
 import Plane from "../models/Plane";
+import HomeInfo from "../components/HomeInfo";
 
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
@@ -48,10 +49,9 @@ const Home = () => {
   return (
     <div>
       <section className="w-full h-screen relative">
-        {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-          {" "}
-          pop
-        </div> */}
+        <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+          {currentStage && <HomeInfo currentStage={currentStage} />}
+        </div>
 
         <Canvas
           className={`w-full h-screen bg-transparent ${
