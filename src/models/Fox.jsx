77ec0a9c21983b89d5737e0e7ec858cbe0f9b978
Mehak <mehakmattoo7@gmail.com>
2024-06-06 18:17:36 +0530

    Integrated email.js in contact section

---
+++ src/models/Fox.jsx
@@ -0,0 +1,47 @@
+import React, { useRef } from "react";
+import { useGLTF, useAnimations } from "@react-three/drei";
+
+export function Model(props) {
+  const group = useRef();
+  const { nodes, materials, animations } = useGLTF("/fox.glb");
+  const { actions } = useAnimations(animations, group);
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <group name="Sketchfab_Scene">
+        <primitive object={nodes.GLTF_created_0_rootJoint} />
+        <skinnedMesh
+          name="Object_7"
+          geometry={nodes.Object_7.geometry}
+          material={materials.PaletteMaterial001}
+          skeleton={nodes.Object_7.skeleton}
+        />
+        <skinnedMesh
+          name="Object_8"
+          geometry={nodes.Object_8.geometry}
+          material={materials.PaletteMaterial001}
+          skeleton={nodes.Object_8.skeleton}
+        />
+        <skinnedMesh
+          name="Object_9"
+          geometry={nodes.Object_9.geometry}
+          material={materials.PaletteMaterial001}
+          skeleton={nodes.Object_9.skeleton}
+        />
+        <skinnedMesh
+          name="Object_10"
+          geometry={nodes.Object_10.geometry}
+          material={materials.PaletteMaterial001}
+          skeleton={nodes.Object_10.skeleton}
+        />
+        <skinnedMesh
+          name="Object_11"
+          geometry={nodes.Object_11.geometry}
+          material={materials.PaletteMaterial001}
+          skeleton={nodes.Object_11.skeleton}
+        />
+      </group>
+    </group>
+  );
+}
+
+useGLTF.preload("/fox.glb");
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,141 @@
 import React from "react";
+import emailjs from "@emailjs/browser";
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useRef, useState } from "react";
+
+// import { Fox } from "../models";
+// import useAlert from "../hooks/useAlert";
+// import { Alert, Loader } from "../components";
 
 const Contact = () => {
-  return <div>contact</div>;
+  const formRef = useRef();
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  // const { alert, showAlert, hideAlert } = useAlert();
+  const [loading, setLoading] = useState(false);
+  const [currentAnimation, setCurrentAnimation] = useState("idle");
+
+  const handleChange = ({ target: { name, value } }) => {
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleFocus = () => setCurrentAnimation("walk");
+  const handleBlur = () => setCurrentAnimation("idle");
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // not reload the page after submission
+    setLoading(true);
+    setCurrentAnimation("hit");
+
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "Mehak Mattoo",
+          from_email: form.email,
+          to_email: "mehakmattoo7@gmail.com",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          setLoading(false);
+          // showAlert({
+          //   show: true,
+          //   text: "Message Sent 😃",
+          //   type: "success",
+          // });
+
+          setTimeout(() => {
+            // hideAlert(false);
+            // setCurrentAnimation("idle");
+            setForm({
+              name: "",
+              email: "",
+              message: "",
+            });
+          }, [3000]);
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+          // setCurrentAnimation("idle");
+
+          // showAlert({
+          //   show: true,
+          //   text: "I didn't receive your message 😢",
+          //   type: "danger",
+          // });
+        }
+      );
+  };
+
+  return (
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in Touch</h1>
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="w-full flex flex-col gap-7 mt-14"
+        >
+          <label className="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="Your Name"
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Email
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="Your Email"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Your Message
+            <textarea
+              name="message"
+              rows="4"
+              className="textarea"
+              placeholder="Write your thoughts here..."
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <button
+            type="submit"
+            disabled={loading}
+            className="btn"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+            {loading ? "Sending..." : "Submit"}
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
