982ec1a32662fda90480d446719e9de630d0b2a6
Mehak <mehakmattoo7@gmail.com>
2024-06-06 10:51:36 +0530

    Added initial files and created the preliminary island

---
+++ src/models/Island.jsx
@@ -0,0 +1,46 @@
+import React, { useRef, useEffect } from "react";
+import { useGLTF } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
+import islandScene from "../assets/3d/island.glb";
+import { a } from "@react-spring/three";
+
+export function Island(props) {
+  const islandRef = useRef();
+
+  const { nodes, materials } = useGLTF(islandScene);
+
+  return (
+    <a.group ref={islandRef} {...props}>
+      <mesh
+        geometry={nodes.polySurface944_tree_body_0.geometry}
+        material={materials.PaletteMaterial001}
+      />
+      <mesh
+        geometry={nodes.polySurface945_tree1_0.geometry}
+        material={materials.PaletteMaterial001}
+      />
+      <mesh
+        geometry={nodes.polySurface946_tree2_0.geometry}
+        material={materials.PaletteMaterial001}
+      />
+      <mesh
+        geometry={nodes.polySurface947_tree1_0.geometry}
+        material={materials.PaletteMaterial001}
+      />
+      <mesh
+        geometry={nodes.polySurface948_tree_body_0.geometry}
+        material={materials.PaletteMaterial001}
+      />
+      <mesh
+        geometry={nodes.polySurface949_tree_body_0.geometry}
+        material={materials.PaletteMaterial001}
+      />
+      <mesh
+        geometry={nodes.pCube11_rocks1_0.geometry}
+        material={materials.PaletteMaterial001}
+      />
+    </a.group>
+  );
+}
+
+export default Island;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,54 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import Loader from "../components/Loader";
+import Island from "../models/Island";
+
+const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      // larger screen
+      screenScale = [1, 1, 1];
+    }
+    return [screenScale, screenPosition, rotation];
+  };
+
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForScreenSize();
+
+  return (
+    <div>
+      <section className="w-full h-screen relative">
+        {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+          {" "}
+          pop
+        </div> */}
+
+        <Canvas
+          className="w-full h-screen bg-transparent"
+          camera={{ near: 0.1, far: 1000 }}
+        >
+          <Suspense fallback={<Loader />}>
+            <directionalLight />
+            <ambientLight />
+            <pointLight />
+            <spotLight />
+            <hemisphereLight />
+            <Island
+              position={islandPosition}
+              scale={islandScale}
+              rotation={islandRotation}
+            />
+          </Suspense>
+        </Canvas>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
