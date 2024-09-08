17e337b6a5637b859d8dd644db2141b1e15ef85f
Mehak <mehakmattoo7@gmail.com>
2024-09-08 15:28:01 +0530

    resolved imports

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,6 @@
 import chegg from "./chegg.png";
 import growtharrow from "./growtharrow.jpg";
 import fliprobo from "./fliprobo.jpg";
+import summarizeX from "./summarizex.png";
 
-export { chegg, growtharrow, fliprobo };
+export { chegg, growtharrow, fliprobo, summarizeX };
